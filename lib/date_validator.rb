3a8f1ac39e10b00ff9694a4b9ad27722e6513776
Kyle Laughlin <kylejlaughlin@icloud.com>
2016-03-14 17:34:20 -0500

    Clean up commented out code.

--- lib/date_validator.rb
+++ lib/date_validator.rb
@@ -46,47 +46,6 @@ def valid_date?(month, day, year)
         return false
       end
     end
-    true
-end
-
-=begin
-#handle february leap year posibility
-  if month = 2
-    if year % 4 != 0
-      leap_year = 0
-    elsif year % 100 != 0
-      leap_year = 1
-    elsif year % 400 != 0
-      leap_year = 0
-    else
-      leap_year = 1
-    end
-    if leap_year = 1
-      if day < 1 || day > 29
-        return false
-      end
-    else
-      if day < 1 || day > 28
-        return false
-      end
-    end
-  else
-    #handle number of days in any other month
-    case month
-    when 1, 3, 5, 7, 8, 10, 12
-      if day < 1 || day > 31
-        return false
-      end
-    when 4, 6, 9, 11
-      if day < 1 || day > 30
-        return false
-      end
-    when 2
-
-    end
   end
-  return true
+  true
 end
-
-valid_date?(3,40,2006)
-=end
